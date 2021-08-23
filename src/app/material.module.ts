274f113a31a2876cc697cee4637bd5a97d3fca08
Deivis Raposo <deivisraposo@gmail.com>
2021-08-23 20:59:59 -0300

    initial commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,66 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+<<<<<<< HEAD
+import { MatRadioModule } from '@angular/material/radio';
+
+
+=======
+import { MatSelectModule } from '@angular/material/select';
+>>>>>>> ac2a4e74be9241f2bd31e7a574377420ce75d5a5
+
+
+@NgModule({
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatCardModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatTreeModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSnackBarModule,
+<<<<<<< HEAD
+    MatRadioModule
+=======
+    MatSelectModule
+>>>>>>> ac2a4e74be9241f2bd31e7a574377420ce75d5a5
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatCardModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatTreeModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSnackBarModule,
+<<<<<<< HEAD
+    MatRadioModule
+=======
+    MatSelectModule
+>>>>>>> ac2a4e74be9241f2bd31e7a574377420ce75d5a5
+  ]
+})
+export class MaterialModule { }
