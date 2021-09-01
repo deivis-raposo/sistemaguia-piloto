cbc6ffee0404d9726d3e4f4e5c6d3a10021a0550
Deivis Raposo <deivisraposo@gmail.com>
2021-09-01 20:31:36 -0300

    initial commit

---
+++ src/app/pipes/cpf.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+@Pipe({
+    name: 'cpf'
+})
+export class CpfPipe implements PipeTransform {
+    transform(value: string, zenkaku: boolean = true): string {
+        if (!value && value.length < 10) {
+            return value;
+        }
+        if (value.length < 12) {
+            return `${value.substr(0, 3)}.${value.substr(3, 3)}.${value.substr(6, 3)}-${value.substr(-1)}`
+        } else {
+            return `${value.substr(0, 2)}.${value.substr(2, 3)}.${value.substr(5, 3)}/${value.substr(8, 4)}-${value.substr(12)}`
+        }
+    }
+}
