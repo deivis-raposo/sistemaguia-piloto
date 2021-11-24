d5c7148469b61b94015b4ac69a52b5b5a22d20df
matheus_moreira <matheusmoreiramds325@gmail.com>
2021-11-24 12:42:25 -0300

    melhoria feita tela relatorios

--- src/app/relatorios/relatorios.component.ts
+++ src/app/relatorios/relatorios.component.ts
@@ -6,19 +6,63 @@ import { RelatorioExtratoMovicombustivelExportComponent } from '../relatorio-ext
 import { RelatorioVendaDiariaProdutosExportComponent } from '../relatorio-venda-diaria-produtos-export/relatorio-venda-diaria-produtos-export.component';
 import { RelatorioVendaDiariaCombustivelComponent } from '../relatorio-venda-diaria-combustivel/relatorio-venda-diaria-combustivel.component';
 import { RelatorioVendaCartoesDiaComponent } from '../relatorio-venda-cartoes-dia/relatorio-venda-cartoes-dia.component';
+import { trigger, transition, animate, style, state, stagger } from '@angular/animations'
 @Component({
   selector: 'app-relatorios',
   templateUrl: './relatorios.component.html',
-  styleUrls: ['./relatorios.component.css']
+  styleUrls: ['./relatorios.component.css'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        opacity: '1'
+
+      })),
+      state('closed', style({
+        opacity: '0'
+      })),
+      transition('open => closed', [
+
+        animate('1.2s')
+      ]),
+      transition('closed => open', [
+        animate('0.8s')
+      ])
+
+    ]),
+    trigger('openCloses', [
+      // ...
+
+      state('open', style({
+        opacity: '0'
+
+      })),
+      state('closed', style({
+        opacity: '1'
+      })),
+      transition('open => closed', [
+        animate('1.2s')
+
+
+      ]),
+      transition('closed => open', [
+        animate('0.8s')
+
+      ])
+
+    ]),
+  ],
+
 })
 export class RelatoriosComponent implements OnInit {
-  panelOpenState = false;
 
 
   constructor(private dialog: MatDialog, private snackBarService: SnackBarService) { }
 
   ngOnInit(): void {
   }
+
+
 
   public relatorioVendaCategoria() {
     this.dialog.open(RelatorioVendacategoriaExportComponent, {
@@ -71,19 +115,43 @@ export class RelatoriosComponent implements OnInit {
   }
 
 
+  /*Muda o display de acordo com qual relatórios foi selecionado*/
+  isOpen = true;
 
-  /*
-  public filtroReport() {
-    this.dialog.open(RelatorioModeloEditComponent, {
-      disableClose: true
-    }).afterClosed().subscribe(resp => {
-      if (resp) {
-        //this.loadAllUser();
-        this.snackBarService.showSnackBar('Relatório gerado com sucesso!', 'OK');
+  public tipoRelatorio: string = 'block';
+  public relatorioProdutos: string = 'none';
+  public relatorioFinanceiro: string = 'none';
+
+  public mudarRelatorio(tprelatorio: string) {
+
+
+
+    this.isOpen = !this.isOpen;
+
+    switch (tprelatorio) {
+      case 'produtos': {
+        this.tipoRelatorio = 'none';
+        this.relatorioProdutos = 'block';
+        break;
       }
-    })
+      case 'financeiro': {
+        this.tipoRelatorio = 'none';
+        this.relatorioFinanceiro = 'block';
+        break;
+      }
+
+
+      case 'fechar': {
+        this.relatorioProdutos = 'none';
+        this.relatorioFinanceiro = 'none';
+        this.tipoRelatorio = 'block';
+        break;
+      }
+      default:
+        break;
+    }
+
+
   }
 
-
-  */
 }
