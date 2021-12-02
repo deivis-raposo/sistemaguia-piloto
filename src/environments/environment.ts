375ae442f1e02b141105781791584f9eca85405d
deivis-raposo <deivisraposo@gmail.com>
2021-12-02 08:44:03 -0300

    atualizacao do front

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:8080/api_piloto/'
-  //baseUrl: 'http://18.230.61.76:8080/api_piloto/'
+  //baseUrl: 'http://localhost:8080/'
+  baseUrl: 'https://18.230.61.76:8080/api_piloto/'
 };
 
